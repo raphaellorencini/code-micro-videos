45d5e05196a786625277ba2395c42c26fdab8c8d
Raphael Lorencini <raphael.lorencini@ish.com.br>
2020-10-22 22:42:36 -0300

    Configuração CI - #16

--- .docker/app/entrypoint.prod.sh
+++ .docker/app/entrypoint.prod.sh
@@ -2,5 +2,6 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data /var/www
-
+php /var/www/artisan config:clear
+php /var/www/artisan cache:clear
 php-fpm
