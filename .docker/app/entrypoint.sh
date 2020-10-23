5219b0d6aafd60277ee4ffa37ae46b303dd49150
Raphael Lorencini <raphael.lorencini@ish.com.br>
2020-10-22 21:35:57 -0300

    Configuração CI - #8

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-chown -R www-data:www-data .
-cp .env.example .env
-cp .env.testing.example .env.testing
-composer install
+chown -R www-data:www-data /var/www
+cp /var/www/template/.env.example /var/www/.env
+cp /var/www/template/.env.testing.example /var/www/.env.testing
+#composer install
 #php artisan key:generate
-php artisan migrate
+#php artisan migrate
 
 php-fpm
