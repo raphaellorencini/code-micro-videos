53708cb913dea74d90f1a483053e682fd68e0601
Raphael Lorencini <raphaellorencini@gmail.com>
2021-04-26 07:10:54 -0300

    Definindo layout do frontend - 1

---
+++ www/frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {AppBar, Button, makeStyles, Theme, Toolbar, Typography} from "@material-ui/core";
+import logo from '../../static/img/logo.png';
+
+const useStyles = makeStyles((theme: Theme) => ({
+    toolbar: {
+        backgroundColor: '#000'
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center'
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')]: {
+            width: 170
+        }
+    }
+}));
+
+export const Navbar: React.FC = () => {
+    const classes = useStyles();
+    return (
+        <AppBar>
+            <Toolbar className={classes.toolbar}>
+                <Typography className={classes.title}>
+                    <img src={logo} className={classes.logo} alt="Codeflix"/>
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
